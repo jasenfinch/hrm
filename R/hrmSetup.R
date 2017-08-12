02e8aad2c79005ca0faefc73d4f80419fed1daf3
jasenfinch <jsf9@aber.ac.uk>
2017-08-12 17:10:26 +0100

    hrmSetup console output

--- R/hrmSetup.R
+++ R/hrmSetup.R
@@ -7,22 +7,15 @@ hrmSetup <- function() {
     p <- packages$Package[!(packages$Package %in% installedPackages$Package)]
     
     if (length(p) > 0) {
-        installBioconductor <- function(package) {
-            BiocInstaller::biocLite(package)
-        }
-        
         installGithub <- function(profile,package) {
-            devtools::install_github(stringr::str_c(profile,package,sep = '/'))
+            suppressMessages(devtools::install_github(stringr::str_c(profile,package,sep = '/')))
         }
         
         walk(p,~{
             package <- packages[packages$Package == .,]
-            
-            if (package$Repository == 'bioconductor') {
-                installBioconductor(package$Package)
-            }
-            
+
             if (package$Repository == 'github') {
+                cat('Installing hrm:',package$Package,'\n',sep = ' ')
                 installGithub(package$Profile,package$Package)
             }
             Sys.sleep(2)
