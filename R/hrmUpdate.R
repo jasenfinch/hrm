72b84089dde9129b43ac962522429c9efb6dba8b
jasenfinch <jsf9@aber.ac.uk>
2017-08-15 20:52:20 +0100

    reverted to old hrmUpdate for console output

--- R/hrmUpdate.R
+++ R/hrmUpdate.R
@@ -2,5 +2,15 @@
 #' @importFrom tibble tibble
 
 hrmUpdate <- function() {
-    update_packages(packages$Package)
+    walk(packages$Package,~{
+        package <- packages[packages$Package == .,]
+        
+        if (package$Repository == 'github') {
+            success <- installGithub(package$Profile,package$Package)
+            if (success == T) {
+                cat('Updating hrm:',package$Package,'\n',sep = ' ')
+            }
+        }
+        Sys.sleep(2)
+    })
 }
