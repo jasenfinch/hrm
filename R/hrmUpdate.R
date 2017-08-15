1368aad5de60a333ccb5d43dd3a05c2fd1af8659
jasenfinch <jsf9@aber.ac.uk>
2017-08-15 20:49:48 +0100

    hrmUpdate now uses update_packages

--- R/hrmUpdate.R
+++ R/hrmUpdate.R
@@ -1,15 +1,6 @@
+#' @importFrom dplyr bind_rows
+#' @importFrom tibble tibble
 
 hrmUpdate <- function() {
-
-        walk(packages$Package,~{
-            package <- packages[packages$Package == .,]
-            
-            if (package$Repository == 'github') {
-                success <- installGithub(package$Profile,package$Package)
-                if (success == T) {
-                    cat('Updating hrm:',package$Package,'\n',sep = ' ')
-                }
-            }
-            Sys.sleep(2)
-        })
+    update_packages(packages$Package)
 }
