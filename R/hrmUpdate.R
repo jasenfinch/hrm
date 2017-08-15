ca2223a6bacdf9d59a29cebc7fd124345f6ad52f
jasenfinch <jsf9@aber.ac.uk>
2017-08-15 20:24:10 +0100

    all package updates now attempted

--- R/hrmUpdate.R
+++ R/hrmUpdate.R
@@ -1,18 +1,15 @@
 
 hrmUpdate <- function() {
-    
-    packagesUpdate <- devtools::package_deps()$package
-    p <- packages$Package[packages$Package %in% packagesUpdate]
-    
-    if (length(p) > 0) {
-        walk(p,~{
+
+        walk(packages$Package,~{
             package <- packages[packages$Package == .,]
             
             if (package$Repository == 'github') {
-                cat('Updating hrm:',package$Package,'\n',sep = ' ')
-                installGithub(package$Profile,package$Package)
+                success <- installGithub(package$Profile,package$Package)
+                if (success == T) {
+                    cat('Updating hrm:',package$Package,'\n',sep = ' ')
+                }
             }
             Sys.sleep(2)
         })
-    }
 }
