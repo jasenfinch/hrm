3c4e2e0bbc766aad7ed3bd771ee0a1ed9ec9e66c
jasenfinch <jsf9@aber.ac.uk>
2017-12-04 14:53:13 +0000

    added bioconductor dependencies

--- R/hrmSetup.R
+++ R/hrmSetup.R
@@ -8,7 +8,14 @@
 hrmSetup <- function() {
     installedPackages <- tibble::as_tibble(installed.packages())
     
+    if (!('BiocInstaller' %in% installedPackages$Package)) {
+        source("https://bioconductor.org/biocLite.R")
+        biocLite()
+    }
+        
     p <- packages$Package[!(packages$Package %in% installedPackages$Package)]
+    
+    
     
     if (length(p) > 0) {
         walk(p,~{
@@ -18,7 +25,19 @@ hrmSetup <- function() {
                 cat('Installing hrm:',package$Package,'\n',sep = ' ')
                 installGithub(package$Profile,package$Package)
             }
+            
+            if (package$Repository == 'bioconductor') {
+                biocLite(package$Package)
+            }
             Sys.sleep(2)
         })
     }
 }
+
+bioconductor <- tibble::tibble(Repository = 'bioconductor',
+                       Profile = NA,
+                       Package = c('impute','mzR','xcms'),
+                       Load = FALSE)
+packages <- dplyr::bind_rows(packages,bioconductor)
+
+save(packages,file = 'R/sysdata.rda')
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -3,6 +3,21 @@ context('Test Functions')
 
 test_that('hrmSetup works',{
     hrmSetup()
+    installedPackages <- tibble::as_tibble(installed.packages())
+    expect_true('hrm' %in% installedPackages$Package)
+    expect_true('binneR' %in% installedPackages$Package)
+    expect_true('binneRlyse' %in% installedPackages$Package)
+    expect_true('FIEmspro' %in% installedPackages$Package)
+    expect_true('metaboData' %in% installedPackages$Package)
+    expect_true('metabolyseR' %in% installedPackages$Package)
+    expect_true('profilePro' %in% installedPackages$Package)
+    expect_true('mzAnnotation' %in% installedPackages$Package)
+    expect_true('MFassign' %in% installedPackages$Package)
+    expect_true('metaboWorkflows' %in% installedPackages$Package)
+    expect_true('metaboVis' %in% installedPackages$Package)
+    expect_true('impute' %in% installedPackages$Package)
+    expect_true('mzR' %in% installedPackages$Package)
+    expect_true('xcms' %in% installedPackages$Package)
 })
 
 test_that('hrmUpdate works',{
