2da91d98a193455dbd1bf78866b17b1e75c5e955
jasenfinch <jsf9@aber.ac.uk>
2017-12-04 12:42:31 +0000

    added function tests

---
+++ tests/testthat/test-functions.R
@@ -0,0 +1,14 @@
+
+context('Test Functions')
+
+test_that('hrmSetup works',{
+    hrmSetup()
+})
+
+test_that('hrmUpdate works',{
+    hrmUpdate()
+})
+
+test_that('hrmAttach works',{
+    hrmAttach()
+})
