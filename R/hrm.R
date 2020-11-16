64861cee96e9fa8118b744660e0b474b2317ab40
jasenfinch <jsf9@aber.ac.uk>
2020-11-16 19:36:25 +0000

    check fixes

---
+++ R/hrm.R
@@ -0,0 +1,18 @@
+globalVariables('.')
+
+## Suppress imports check notes
+#' @importFrom metaboData availableDataSets
+#' @importFrom binneR readFiles
+#' @importFrom metabolyseR analysisData
+#' @importFrom profilePro profileParameters
+#' @importFrom mzAnnotation convert
+#' @importFrom MFassign assignmentParameters
+#' @importFrom construction construction
+#' @importFrom composition keggPathways
+#' @importFrom riches enrichmentParameters
+#' @importFrom metaboMisc preTreatModes
+#' @importFrom metaboWorkflows workflowParameters
+#' @importFrom metaboReports report
+#' @importFrom tidyverse tidyverse_packages
+
+NULL
